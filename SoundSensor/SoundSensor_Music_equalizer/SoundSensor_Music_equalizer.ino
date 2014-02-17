15134c6829d11f977261fea8d52ab5ecb731912b
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-02-17 07:51:50 +0100

    blinkarray

--- SoundSensor/SoundSensor_Music_equalizer/SoundSensor_Music_equalizer.ino
+++ SoundSensor/SoundSensor_Music_equalizer/SoundSensor_Music_equalizer.ino
@@ -1,21 +1,21 @@
 int DO=2;  //Pin for Digital Output - DO
 int DA=A0;  // Pin for Analog Output - AO
 int led = 13;
-int threshold = 499; //Change this
+int threshold = 525; //Set minimum threshold for LED lit
 int sensorvalue=0;
 
 void setup(){
-//  Serial.begin(9600);
+  Serial.begin(9600);
   pinMode(led, OUTPUT);    
 }
 
 void loop(){
   sensorvalue = analogRead(DA);  //Read the analog value
-//  Serial.print("Analog: ");  
-//  Serial.print(sensorvalue);  //Print the analog value
-//  Serial.print("  ");
-//  Serial.print("Digital: ");
-//  Serial.println(digitalRead(DO));  //Print the digital value
+  Serial.print("Analog: ");  
+  Serial.print(sensorvalue);  //Print the analog value
+  Serial.print("  ");
+  Serial.print("Digital: ");
+  Serial.println(digitalRead(DO));  //Print the digital value
 
   if(sensorvalue >= threshold){  //Compare analog value with threshold 
     digitalWrite(led, HIGH);  //Turn ON Led
