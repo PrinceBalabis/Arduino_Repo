f05a076d024cbd8ad81d27df6f6b0f4fb0098d99
Prince <princebalabis@gmail.com>
2015-07-16 11:11:52 +0200

    Removed some things

--- Projects/HomeAutomation/examples/HomeNetworkExampleServer/Thread2.ino
+++ Projects/HomeAutomation/examples/HomeNetworkExampleServer/Thread2.ino
@@ -6,20 +6,18 @@
 static msg_t Thread2(void *arg) {
   chThdSleepMilliseconds(3000); // Give some time for HomeNetwork thread to start
 
-  //uint32_t count = 0;
-
   while (1) {
     if (msgReceived) {
       if (msgContent == cmdExampleCommand) {
         // Send return-message back to client
         bool msgSent = homeNetwork.responseExampleDataToClient(msgSender, cmdExampleResponceData);
         if (msgSent) {
-          Serial.println(F("Responded"));
+          Serial.println(F("Answered"));
         } else if (!msgSent) {
           Serial.println(F("Timeout"));
         }
       }
-      msgReceived = 0;
+      msgReceived = false;
     }
     chThdSleepMilliseconds(40); // Check every few ms if a message is received
   }
