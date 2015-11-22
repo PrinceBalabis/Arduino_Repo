ae38e4eb3d10ccedc000d5d1d12524fbaf67f8d1
Prince <princebalabis@gmail.com>
2015-11-22 05:21:15 +0100

    coin toggling done

--- Projects/CoinCounter/Buttons.ino
+++ Projects/CoinCounter/Buttons.ino
@@ -39,7 +39,20 @@ NIL_THREAD(Buttons, arg) {
   }
 }
 
+// Function which toggles between coin values
 void toggleCoinValue() {
-  coinValue = 10;
+  if (coinValue == 0.50) {
+    coinValue = 1; // Set to 1 kr
+  } else if (coinValue == 1) {
+    coinValue = 2; // Set to 2 kr
+  } else if (coinValue == 2) {
+    coinValue = 5; // Set to 5 kr
+  } else if (coinValue == 5) {
+    coinValue = 10; // Set to 10 kr
+  } else if (coinValue == 10) {
+    coinValue = 20; // Set to 20 kr
+  } else if (coinValue == 20) {
+    coinValue = 0.50; // Set to 0.50 kr
+  }
 }
 
