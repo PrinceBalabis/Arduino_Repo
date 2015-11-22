cd7744c5136f86e7bbf702f1a2eb55370728a3cd
Prince <princebalabis@gmail.com>
2015-11-22 09:04:54 +0100

    tweaks

--- Projects/CoinCounter/config.h
+++ Projects/CoinCounter/config.h
@@ -16,12 +16,12 @@ SCL - ANALOG pin 5
 #define PHOTO_DIODE_PIN A0  //define a pin for Photo resistor
 #define PHOTO_DIODE_SENSOR_THRESHOLD 2  //if its below this value, a coin is being dropped, if its above, then there is no coin being dropped
 #define COINSENSOR_POLLING_TIME 1 //get sensor value every chosen millisecond
-#define COINSENSOR_LAG_TIME 100 // Sleep a little in order to correct bug where the empty coin circle in danish coins will make the coin counter think its two coins.
+#define COINSENSOR_LAG_TIME 50 // Sleep a little in order to correct bug where the empty coin circle in danish coins will make the coin counter think its two coins.
 
 // LED Buzzer config
 #define LED_BUZZER_PIN 2  //define a pin for LED which blinks while counting
-#define LED_BUZZER_BEEP_LENGTH 50
-#define LED_BUZZER_BEEP_SILENT_DIVIDER 50
+#define LED_BUZZER_BEEP_LENGTH 60
+#define LED_BUZZER_BEEP_SILENT_DIVIDER 60
 
 // Buttons config
 #define BUTTON_TOGGLE_COIN_VALUE_PIN 8  //define a pin for the button which toggles coin value
