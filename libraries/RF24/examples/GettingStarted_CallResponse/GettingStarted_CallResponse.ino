c20ef960fbbbd8dffab6494343255d46931ddc85
Prince <princebalabis@gmail.com>
2015-11-28 14:42:22 +0100

    updated example sketches

--- libraries/RF24/examples/GettingStarted_CallResponse/GettingStarted_CallResponse.ino
+++ libraries/RF24/examples/GettingStarted_CallResponse/GettingStarted_CallResponse.ino
@@ -20,7 +20,7 @@
 bool radioNumber = 0;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
+RF24 radio(8,9);
 /**********************************************************/
                                                                            // Topology
 byte addresses[][6] = {"1Node","2Node"};              // Radio pipe addresses for the 2 nodes to communicate.
