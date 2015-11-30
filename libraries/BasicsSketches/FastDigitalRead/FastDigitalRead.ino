d656f611ed639f5dc9a146a671ebca86846879f3
Prince <princebalabis@gmail.com>
2015-11-30 05:22:20 +0100

    Better fast digital codes

--- libraries/BasicsSketches/FastDigitalRead/FastDigitalRead.ino
+++ libraries/BasicsSketches/FastDigitalRead/FastDigitalRead.ino
@@ -4,23 +4,36 @@
 
    Atmega328 pin mapping:
    http://www.jameco.com/Jameco/workshop/JamecoBuilds/arduinocircuit_fig12.jpg
+
+    More to read:
+   http://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit-in-c-c
 */
+
+#define PIN_1 4
+#define PIN_1_INPUT_INIT (DDRD &= ~_BV(PIN_1))
+#define PIN_1_SET_PULLUP (PORTD |= _BV(PIN_1))
+#define PIN_1_READ (PIND & _BV(4) > 0)
+
+#define PIN_2 5
+#define PIN_2_INPUT_INIT (DDRD &= ~_BV(PIN_2))
+#define PIN_2_SET_PULLUP (PORTD |= _BV(PIN_2))
+#define PIN_2_READ (PIND & _BV(5) > 0)
 
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Started reading digital pin"));
 
-  DDRD = ~(1 << 4); // Sets pin 4 as input
-  DDRD = ~(1 << 5); // Sets pin 5 as input
+  PIN_1_INPUT_INIT; // Sets pin 4 as input
+  PIN_2_INPUT_INIT; // Sets pin 5 as input
   // DDRD = ~B00110000 // This can be used instead to set both pin 4 and 5 as input
 
-  PORTD = (1 << 4); // Set pullup
-  PORTD = (1 << 5); // Set pullup
+  PIN_1_SET_PULLUP; // Set pullup
+  PIN_2_SET_PULLUP; // Set pullup
 }
 
 void loop() {
-  Serial.print(PIND & (1 << 4) > 0);
+  Serial.print(PIN_1_READ);
   Serial.print(F(" -- "));
-  Serial.println(PIND & (1 << 5) > 0);
+  Serial.println(PIN_2_READ);
   delay(100);
 }
--- libraries/BasicsSketches/FastDigitalWrite/FastDigitalWrite.ino
+++ libraries/BasicsSketches/FastDigitalWrite/FastDigitalWrite.ino
@@ -1,18 +1,42 @@
 /**
- * This Sketch gives an example how to blink LED on pin 13
- * on and off
- * 
- * Atmega328 pin mapping:
- * http://www.jameco.com/Jameco/workshop/JamecoBuilds/arduinocircuit_fig12.jpg
- */
+   This Sketch gives an example how to blink LED on pin 13 and pin 2
+   on and off
+
+   Atmega328 pin mapping:
+   http://www.jameco.com/Jameco/workshop/JamecoBuilds/arduinocircuit_fig12.jpg
+
+   More to read:
+   http://stackoverflow.com/questions/47981/how-do-you-set-clear-and-toggle-a-single-bit-in-c-c
+*/
+#define LED_13_PIN 5
+#define LED_13_OUTPUT_INIT (DDRB |= _BV(LED_13_PIN))
+#define LED_13_ON (PORTB |= _BV(LED_13_PIN))
+#define LED_13_OFF (PORTB &= ~_BV(LED_13_PIN))
+#define LED_13_TOGGLE (PORTB ^= _BV(LED_13_PIN))
+
+
+#define LED_2_PIN 2
+#define LED_2_OUTPUT_INIT (DDRD |= _BV(LED_2_PIN))
+#define LED_2_ON (PORTD |= _BV(LED_2_PIN))
+#define LED_2_OFF (PORTD &= ~_BV(LED_2_PIN))
+#define LED_2_TOGGLE (PORTD ^= _BV(LED_2_PIN))
 
 void setup() {
-   DDRB = (1 << 5); // Set LED on board as output
+  LED_13_OUTPUT_INIT; // Set LED on pin 13 as output
+  LED_2_OUTPUT_INIT; // Set LED on pin 2 as output
 }
 
 void loop() {
-  PORTB = (1 << 5); // Toggle on
+  LED_13_ON; // Set pin 13 on
+  LED_2_ON; // Set pin 2 on
   delay(1000);
-  PORTB = ~(1 << 5); // Toggle off
+  LED_13_OFF; // Set pin 13 off
+  LED_2_OFF; // Set pin 2 off
+  delay(1000);
+  LED_13_TOGGLE; // Toggle pin 13, in this case it sets ON
+  LED_2_TOGGLE; // Toggle pin 2, in this case it sets ON
+  delay(1000);
+  LED_13_TOGGLE; // Toggle pin 13, in this case it sets OFF
+  LED_2_TOGGLE; // Toggle pin 2, in this case it sets OFF
   delay(1000);
 }
