c99ab23738ea47bd76fb5a08ea7e832d449cadf8
Prince <princebalabis@gmail.com>
2015-12-14 03:10:12 +0100

    Central node stability update

--- Projects/HomeAutomation/CentralNode/CentralNode.ino
+++ Projects/HomeAutomation/CentralNode/CentralNode.ino
@@ -9,20 +9,32 @@
 RF24 radio(8, 9);
 RF24Network network(radio);
 
+#define LED_13_PIN 5
+#define LED_13_OUTPUT_INIT (DDRB |= _BV(LED_13_PIN))
+#define LED_13_ON (PORTB |= _BV(LED_13_PIN))
+#define LED_13_OFF (PORTB &= ~_BV(LED_13_PIN))
+
 void setup() {
   delay(4000); //Give some time for capacitor to charge up and stabilize before initializing RF24 module!
-  pinMode(13, OUTPUT);
+  LED_13_OUTPUT_INIT;
+  delay(100);
+  LED_13_OFF;
   SPI.begin(); // SPI is used by homeNetwork
   delay(500);
   radio.begin(); // Initialize radio
+  delay(100);
   network.begin(90, 00); // Start mesh Network
   delay(500);
   radio.setPALevel(RF24_PA_MAX);
+  delay(100);
   radio.setDataRate(RF24_2MBPS);
+  delay(100);
   network.txTimeout = 0; // Set to 0 to use the normal auto retry period defined by radio.setRetries()
+  delay(100);
   radio.setRetries(15, 15);
   delay(500);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  LED_13_ON;   // turn the LED on (HIGH is the voltage level)
+  delay(100);
 }
 
 void loop() {
