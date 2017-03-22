84fb1561dca19e3a1d3b9a606d5ca1bb42d0887f
Prince Stevie-Ray Charles Balabis <Prince@MacbookEth.lan>
2017-03-22 15:11:15 +0100

    fixed the example for blinking the 6x LED module

--- libraries/_BasicComponent/LED_LED6Module/LED6Module_Blink/LED6Module_Blink.ino
+++ libraries/_BasicComponent/LED_LED6Module/LED6Module_Blink/LED6Module_Blink.ino
@@ -3,34 +3,25 @@
 */
 
 // Define LED pins
-//const int LedGND = 13; // GND pin on the LED module
-const int leds[] = {13, 12, 11, 10, 9, 8}; // Actual pins to toggle the LEDs
-
-//const int L1 = 5; // D1 pin on the LED module
-//const int L2 = 4; // D2 pin on the LED module
-//const int L3 = 3; // D3 pin on the LED module
-//const int L4 = 2; // D4 pin on the LED module
-//const int L5 = 1; // D5 pin on the LED module
-//const int L6 = 0; // D6 pin on the LED module
+const uint8_t leds[] = {13, 12, 11, 10, 9, 8}; // Actual pins to toggle the LEDs
+const uint8_t amountOfLeds = sizeof(leds) / sizeof(uint8_t);
 
 // The setup routine runs once upon start
 void setup() {
-  // Initialize the leds pin as an output.
-  //pinMode(LedGND, OUTPUT);
-  //Set the digital pin to LOW, to emulate a GND pin
-  //digitalWrite(LedGND, LOW);
-
-  for (int i = 0; i < 5; i++) {
+  // Initialize pins connected to LEDs as output
+  for (int i = 0; i < amountOfLeds; i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
+
 // The loop routine runs over and over again forever
 void loop() {
-  for (int i = 0; i < 5; i++) {
+  // Blink the LEDs one at a time
+  for (int i = 0; i < amountOfLeds; i++) {
     digitalWrite(leds[i], HIGH);
-    delay(500);
+    delay(250);
     digitalWrite(leds[i], LOW);
-    delay(500);
+    delay(250);
   }
 }
 
